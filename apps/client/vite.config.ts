535bd5c91e34659445976f990499825126641bb7
Nikita <pozhidaevw@gmail.com>
2024-04-12 15:03:08 +0700

    chore: server static
    - vite.config.ts proxy server
    - app.module.ts server static
    - main.ts prefix name for api interaction
    - test server + client
    - config (package.json)

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -3,4 +3,12 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      "/server": {
+        target: "http://localhost:3000", // local nest app
+        changeOrigin: true,
+      },
+    },
+  },
 });
