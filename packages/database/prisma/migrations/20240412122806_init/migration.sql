1c1f8f7efb8c9c50bbac8d15b26bc665060c6f97
Nikita <pozhidaevw@gmail.com>
2024-05-13 18:50:26 +0700

    refactor: prisma seed and schema

--- packages/database/prisma/migrations/20240412122806_init/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "Chat" (
-    "id" SERIAL NOT NULL,
-    "username" TEXT NOT NULL,
-
-    CONSTRAINT "Chat_pkey" PRIMARY KEY ("id")
-);
--- packages/database/prisma/seed.ts
+++ packages/database/prisma/seed.ts
@@ -2,19 +2,28 @@ import { PrismaClient, Prisma } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
+const roleData: Prisma.RoleCreateInput[] = [
+  {
+    role: "user",
+  },
+  {
+    role: "admin",
+  },
+];
+
 const userData: Prisma.UserCreateInput[] = [
   {
-    name: "User",
+    email: "user@mail.ru",
     login: "User",
     password: "User",
   },
   {
-    name: "User 1",
+    email: "user1@mail.ru",
     login: "User 1",
     password: "User 1",
   },
   {
-    name: "User 2",
+    email: "user2@mail.ru",
     login: "User 2",
     password: "User 2",
   },
@@ -22,12 +31,21 @@ const userData: Prisma.UserCreateInput[] = [
 
 async function main() {
   console.log(`Start seeding ...`);
+
+  for (const r of roleData) {
+    const role = await prisma.role.create({
+      data: r,
+    });
+    console.log(`Created role with id: ${role.id}`);
+  }
+
   for (const u of userData) {
     const user = await prisma.user.create({
       data: u,
     });
     console.log(`Created user with id: ${user.id}`);
   }
+
   console.log(`Seeding finished.`);
 }
 
