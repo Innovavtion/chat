dd7068816af0991732dc0246aa37db6f8f98907b
Nikita <pozhidaevw@gmail.com>
2024-05-15 17:00:25 +0700

    feat: integration bruno api tests and simple code
    - Добавление bruno тестов
    - Добавление простой логики api для проверки bruno
    - Добавление команд в корневой package

--- apps/server/src/app.controller.ts
+++ apps/server/src/app.controller.ts
@@ -1,7 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body, Delete } from '@nestjs/common';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma.service';
-import { User as UserModel } from '@packages/database';
+import { Users as UserModel } from '@packages/database';
 
 @Controller()
 export class AppController {
@@ -17,6 +17,33 @@ export class AppController {
 
   @Get('users')
   async getAllUsers(): Promise<UserModel[]> {
-    return this.prismaService.user.findMany();
+    return this.prismaService.users.findMany();
+  }
+
+  @Get('users/:id')
+  async getUser(@Param('id') id: string): Promise<UserModel[]> {
+    return this.prismaService.users.findMany({ where: { id: Number(id) } });
+  }
+
+  @Post('user')
+  async createUser(
+    @Body()
+    userData: {
+      login: string;
+      email: string;
+      password: string;
+      lastName: string;
+      firstName: string;
+    },
+  ): Promise<UserModel> {
+    const { login, email, password, lastName, firstName } = userData;
+    return this.prismaService.users.create({
+      data: { login, email, password, lastName, firstName },
+    });
+  }
+
+  @Delete('user/:id')
+  async deleteUser(@Param('id') id: string): Promise<UserModel> {
+    return this.prismaService.users.delete({ where: { id: Number(id) } });
   }
 }
